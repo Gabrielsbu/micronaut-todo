f7795352a0e6d7bcf3ab41a6651bf51e5f8f7b24
Gabriel Maia <gabrielmaiasbu@gmail.com>
2021-12-09 15:04:05 -0300

    add swagger

--- src/main/java/br/com/estudo/Application.java
+++ src/main/java/br/com/estudo/Application.java
@@ -1,6 +1,20 @@
 package br.com.estudo;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bem-vindos",
+                version = "1.0",
+                description = "Api responsavel por organizar seu dia!",
+                license = @License(name = "Apache 2.0", url = "https://foo.bar"),
+                contact = @Contact(url = "https://gigantic-server.com", name = "Gabriel", email = "gabrielmaiasbu@gmail.com")
+        )
+)
 
 public class Application {
 
